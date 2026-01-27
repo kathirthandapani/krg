8dacfaf109bf79caf30bbda264329bbec770fde2
boobalan <boobalan1705a@gmail.com>
2026-01-27 19:32:41 +0530

    Listen on 0.0.0.0 for Render compatibility

--- server.js
+++ server.js
@@ -510,6 +510,6 @@ app.post('/api/admin/users', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
